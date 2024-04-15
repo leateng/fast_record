7a9552a39473596fb8865108bcd06401a3a56dd6
teng.li <teng.li@yhlsoft.com>
2024-04-15 08:33:23 +0800

    update

--- ext/fast_record/src/lib.rs
+++ ext/fast_record/src/lib.rs
@@ -1,11 +1,62 @@
 use magnus::{function, method, prelude::*, Error, Ruby};
+use std::convert::Infallible;
+use std::net::SocketAddr;
 
-fn hello() {
-    println!("hello rust");
+use http_body_util::Full;
+use hyper::body::Bytes;
+use hyper::server::conn::http1;
+use hyper::service::service_fn;
+use hyper::{Request, Response};
+use hyper_util::rt::TokioIo;
+use tokio::net::TcpListener;
+
+// fn hello() {
+//     println!("hello rust");
+// }
+
+async fn hello(_: Request<hyper::body::Incoming>) -> Result<Response<Full<Bytes>>, Infallible> {
+    Ok(Response::new(Full::new(Bytes::from("Hello, World!"))))
+}
+
+async fn hello_server() -> Result<(), Box<dyn std::error::Error + Send + Sync>> {
+    let addr = SocketAddr::from(([127, 0, 0, 1], 3000));
+
+    // We create a TcpListener and bind it to 127.0.0.1:3000
+    let listener = TcpListener::bind(addr).await?;
+
+    // We start a loop to continuously accept incoming connections
+    loop {
+        let (stream, _) = listener.accept().await?;
+
+        // Use an adapter to access something implementing `tokio::io` traits as if they implement
+        // `hyper::rt` IO traits.
+        let io = TokioIo::new(stream);
+
+        // Spawn a tokio task to serve multiple connections concurrently
+        tokio::task::spawn(async move {
+            // Finally, we bind the incoming connection to our `hello` service
+            if let Err(err) = http1::Builder::new()
+                // `service_fn` converts our function in a `Service`
+                .serve_connection(io, service_fn(hello))
+                .await
+            {
+                eprintln!("Error serving connection: {:?}", err);
+            }
+        });
+    }
+}
+
+fn hello_server_wrapper() {
+    tokio::runtime::Builder::new_multi_thread()
+        .worker_threads(4)
+        .enable_all()
+        .build()
+        .unwrap()
+        .block_on(hello_server());
 }
 
 #[magnus::init]
 fn init(ruby: &Ruby) -> Result<(), Error> {
-    ruby.define_global_function("hello", function!(hello, 0));
+    ruby.define_global_function("hello", function!(hello_server_wrapper, 0));
     Ok(())
 }
--- lib/fast_record.rb
+++ lib/fast_record.rb
@@ -1,8 +1,9 @@
 # frozen_string_literal: true
 
 require_relative "fast_record/version"
+require_relative "ext/fast_record"
 
-module FastRecord
-  class Error < StandardError; end
-  # Your code goes here...
-end
+# module FastRecord
+#   class Error < StandardError; end
+#   # Your code goes here...
+# end
